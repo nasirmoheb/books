6d3eb77e61ec88dded3a3a34ed52292f7af8af54
Moheb <naisr.moheb111@gmail.com>
2023-07-31 08:50:51 +0430

    fixing the search bar display in rtl languages

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,22 +7,12 @@
   </div>
 
   <!-- Search Modal -->
-  <Modal
-    :open-modal="openModal"
-    :set-close-listener="false"
-    @closemodal="close"
-  >
+  <Modal :open-modal="openModal" :set-close-listener="false" @closemodal="close">
     <div class="w-form">
       <!-- Search Input -->
       <div class="p-1">
-        <input
-          ref="input"
-          v-model="inputValue"
-          type="search"
-          autocomplete="off"
-          spellcheck="false"
-          :placeholder="t`Type to search...`"
-          class="
+        <input ref="input" v-model="inputValue" type="search" autocomplete="off" spellcheck="false"
+          :placeholder="t`Type to search...`" class="
             bg-gray-100
             text-2xl
             focus:outline-none
@@ -31,45 +21,27 @@
             text-gray-900
             rounded-md
             p-3
-          "
-          @keydown.up="up"
-          @keydown.down="down"
-          @keydown.enter="() => select()"
-          @keydown.esc="close"
-        />
+          " @keydown.up="up" @keydown.down="down" @keydown.enter="() => select()" @keydown.esc="close" />
       </div>
       <hr v-if="suggestions.length" />
 
       <!-- Search List -->
       <div :style="`max-height: ${49 * 6 - 1}px`" class="overflow-auto">
-        <div
-          v-for="(si, i) in suggestions"
-          :key="`${i}-${si.label}`"
-          :data-index="`search-suggestion-${i}`"
-          class="hover:bg-gray-50 cursor-pointer"
-          :class="idx === i ? 'border-blue-500 bg-gray-50 border-s-4' : ''"
-          @click="select(i)"
-        >
+        <div v-for="(si, i) in suggestions" :key="`${i}-${si.label}`" :data-index="`search-suggestion-${i}`"
+          class="hover:bg-gray-50 cursor-pointer" :class="idx === i ? 'border-blue-500 bg-gray-50 border-s-4' : ''"
+          @click="select(i)">
           <!-- Search List Item -->
-          <div
-            class="flex w-full justify-between px-3 items-center"
-            style="height: var(--h-row-mid)"
-          >
+          <div class="flex w-full justify-between px-3 items-center"
+            :class="languageDirection === 'rtl' ? 'flex-row-reverse' : ''" style="height: var(--h-row-mid)">
             <div class="flex items-center">
-              <p
-                :class="idx === i ? 'text-blue-600' : 'text-gray-900'"
-                :style="idx === i ? 'margin-left: -4px' : ''"
-              >
+              <p :class="idx === i ? 'text-blue-600' : 'text-gray-900'" :style="idx === i ? 'margin-left: -4px' : ''">
                 {{ si.label }}
               </p>
               <p v-if="si.group === 'Docs'" class="text-gray-600 text-sm ms-3">
                 {{ si.more.filter(Boolean).join(', ') }}
               </p>
             </div>
-            <p
-              class="text-sm text-end justify-self-end"
-              :class="`text-${groupColorMap[si.group]}-500`"
-            >
+            <p class="text-sm text-end justify-self-end" :class="`text-${groupColorMap[si.group]}-500`">
               {{
                 si.group === 'Docs' ? si.schemaLabel : groupLabelMap[si.group]
               }}
@@ -86,20 +58,12 @@
         <!-- Group Filters -->
         <div class="flex justify-between">
           <div class="flex gap-1">
-            <button
-              v-for="g in searchGroups"
-              :key="g"
-              class="border px-1 py-0.5 rounded-lg"
-              :class="getGroupFilterButtonClass(g)"
-              @click="searcher!.set(g, !searcher!.filters.groupFilters[g])"
-            >
+            <button v-for="g in searchGroups" :key="g" class="border px-1 py-0.5 rounded-lg"
+              :class="getGroupFilterButtonClass(g)" @click="searcher!.set(g, !searcher!.filters.groupFilters[g])">
               {{ groupLabelMap[g] }}
             </button>
           </div>
-          <button
-            class="hover:text-gray-900 py-0.5 rounded text-gray-700"
-            @click="showMore = !showMore"
-          >
+          <button class="hover:text-gray-900 py-0.5 rounded text-gray-700" @click="showMore = !showMore">
             {{ showMore ? t`Less Filters` : t`More Filters` }}
           </button>
         </div>
@@ -108,13 +72,9 @@
         <div v-if="showMore" class="-mt-1">
           <!-- Group Skip Filters -->
           <div class="flex gap-1 text-gray-800">
-            <button
-              v-for="s in ['skipTables', 'skipTransactions'] as const"
-              :key="s"
-              class="border px-1 py-0.5 rounded-lg"
-              :class="{ 'bg-gray-200': searcher?.filters[s] }"
-              @click="searcher?.set(s, !searcher?.filters[s])"
-            >
+            <button v-for="s in ['skipTables', 'skipTransactions'] as const" :key="s"
+              class="border px-1 py-0.5 rounded-lg" :class="{ 'bg-gray-200': searcher?.filters[s] }"
+              @click="searcher?.set(s, !searcher?.filters[s])">
               {{
                 s === 'skipTables' ? t`Skip Child Tables` : t`Skip Transactions`
               }}
@@ -123,27 +83,21 @@
 
           <!-- Schema Name Filters -->
           <div class="flex mt-1 gap-1 text-blue-500 flex-wrap">
-            <button
-              v-for="sf in schemaFilters"
-              :key="sf.value"
-              class="
+            <button v-for="sf in schemaFilters" :key="sf.value" class="
                 border
                 px-1
                 py-0.5
                 rounded-lg
                 border-blue-100
                 whitespace-nowrap
-              "
-              :class="{
+              " :class="{
                 'bg-blue-100': searcher?.filters.schemaFilters[sf.value],
-              }"
-              @click="
-                searcher?.set(
-                  sf.value,
-                  !searcher?.filters.schemaFilters[sf.value]
-                )
-              "
-            >
+              }" @click="
+  searcher?.set(
+    sf.value,
+    !searcher?.filters.schemaFilters[sf.value]
+  )
+  ">
               {{ sf.label }}
             </button>
           </div>
@@ -155,10 +109,7 @@
             <p>↑↓ {{ t`Navigate` }}</p>
             <p>↩ {{ t`Select` }}</p>
             <p><span class="tracking-tighter">esc</span> {{ t`Close` }}</p>
-            <button
-              class="flex items-center hover:text-gray-800"
-              @click="openDocs"
-            >
+            <button class="flex items-center hover:text-gray-800" @click="openDocs">
               <feather-icon name="help-circle" class="w-4 h-4 me-1" />
               {{ t`Help` }}
             </button>
@@ -168,21 +119,11 @@
             {{ t`${suggestions.length} out of ${searcher.numSearches}` }}
           </p>
 
-          <div
-            v-if="(searcher?.numSearches ?? 0) > 50"
-            class="border border-gray-100 rounded flex justify-self-end ms-2"
-          >
-            <template
-              v-for="c in allowedLimits.filter(
-                (c) => c < (searcher?.numSearches ?? 0) || c === -1
-              )"
-              :key="c + '-count'"
-            >
-              <button
-                class="w-9"
-                :class="limit === c ? 'bg-gray-100' : ''"
-                @click="limit = Number(c)"
-              >
+          <div v-if="(searcher?.numSearches ?? 0) > 50" class="border border-gray-100 rounded flex justify-self-end ms-2">
+            <template v-for="c in allowedLimits.filter(
+              (c) => c < (searcher?.numSearches ?? 0) || c === -1
+            )" :key="c + '-count'">
+              <button class="w-9" :class="limit === c ? 'bg-gray-100' : ''" @click="limit = Number(c)">
                 {{ c === -1 ? t`All` : c }}
               </button>
             </template>
@@ -196,7 +137,7 @@
 <script lang="ts">
 import { fyo } from 'src/initFyo';
 import { getBgTextColorClass } from 'src/utils/colors';
-import { searcherKey, shortcutsKey } from 'src/utils/injectionKeys';
+import { searcherKey, shortcutsKey, languageDirectionKey } from 'src/utils/injectionKeys';
 import { openLink } from 'src/utils/ipcCalls';
 import { docsPathMap } from 'src/utils/misc';
 import {
@@ -219,6 +160,7 @@ export default defineComponent({
     return {
       searcher: inject(searcherKey),
       shortcuts: inject(shortcutsKey),
+      languageDirection: inject(languageDirectionKey),
     };
   },
   data() {
